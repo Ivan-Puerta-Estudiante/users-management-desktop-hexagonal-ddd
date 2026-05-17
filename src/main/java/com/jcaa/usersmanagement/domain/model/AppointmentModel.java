2a03d7735270afaa7bb4c518445a7d2eaf2ee13e
ivan puerta <ipuertaa@unicartagena.edu.co>
2026-05-17 16:43:20 -0500

    feat: create initial AppointmenModel structure

---
+++ src/main/java/com/jcaa/usersmanagement/domain/model/AppointmentModel.java
@@ -0,0 +1,29 @@
+package com.jcaa.usersmanagement.domain.model;
+
+import lombok.Value;
+@Value
+public class AppointmentModel {
+
+    AppointmentId id;
+    PatientName patientName;
+    DoctorName doctorName;
+    AppointmentDate appointmentDate;
+    AppointmentReason appointmentReason;
+    AppointmentStatus appointmentStatus;
+
+    public static AppointmentModel create(
+            final AppointmentId id,
+            final PatientName patientName,
+            final DoctorName doctorName,
+            final AppointmentDate appointmentDate,
+            final AppointmentReason appointmentReason) {
+        return new AppointmentModel(id, patientName, doctorName, appointmentDate, appointmentReason, AppointmentStatus.SCHEDULED);
+    }
+
+    public AppointmentModel completed() {
+        return new AppointmentModel(id, patientName, doctorName, appointmentDate, appointmentReason, AppointmentStatus.COMPLETED);
+    }
+    public AppointmentModel canceled() {
+        return new AppointmentModel(id, patientName, doctorName, appointmentDate, appointmentReason, AppointmentStatus.CANCELED);
+    }
+}
