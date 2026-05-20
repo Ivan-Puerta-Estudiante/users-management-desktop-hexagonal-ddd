110769d3094257c13d3bd6c5a059402de6de87b0
ivan puerta <ipuertaa@unicartagena.edu.co>
2026-05-20 17:25:13 -0500

    refactor: update AppointmentModel to use patientId and doctorId

--- src/main/java/com/jcaa/usersmanagement/domain/model/AppointmentModel.java
+++ src/main/java/com/jcaa/usersmanagement/domain/model/AppointmentModel.java
@@ -7,25 +7,25 @@ import lombok.Value;
 public class AppointmentModel {
 
     AppointmentId id;
-    PatientName patientName;
-    DoctorName doctorName;
+   // PatientId patientId;  se habilitara cuando la clase relacionada sea creada
+   // DoctorId doctorId;  se habilitara cuando la clase relacionda sea creada
     AppointmentDate appointmentDate;
     AppointmentReason appointmentReason;
     AppointmentStatus appointmentStatus;
 
     public static AppointmentModel create(
             final AppointmentId id,
-            final PatientName patientName,
-            final DoctorName doctorName,
+           // final PatientId patientId,
+           // final DoctorId doctorId,
             final AppointmentDate appointmentDate,
             final AppointmentReason appointmentReason) {
-        return new AppointmentModel(id, patientName, doctorName, appointmentDate, appointmentReason, AppointmentStatus.SCHEDULED);
+        return new AppointmentModel(id, appointmentDate, appointmentReason, AppointmentStatus.SCHEDULED);
     }
 
     public AppointmentModel completed() {
-        return new AppointmentModel(id, patientName, doctorName, appointmentDate, appointmentReason, AppointmentStatus.COMPLETED);
+        return new AppointmentModel(id, appointmentDate, appointmentReason, AppointmentStatus.COMPLETED);
     }
     public AppointmentModel canceled() {
-        return new AppointmentModel(id, patientName, doctorName, appointmentDate, appointmentReason, AppointmentStatus.CANCELED);
+        return new AppointmentModel(id, appointmentDate, appointmentReason, AppointmentStatus.CANCELED);
     }
 }
