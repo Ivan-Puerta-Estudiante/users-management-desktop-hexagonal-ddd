3c2c502d335e8d4a460946ab71563de6c3fdc6dc
ivan puerta <ipuertaa@unicartagena.edu.co>
2026-05-21 15:17:31 -0500

    feat: add AppointmentCreatedDomainEvent and update AppointmentModel with doctorId and patientId

--- src/main/java/com/jcaa/usersmanagement/domain/model/AppointmentModel.java
+++ src/main/java/com/jcaa/usersmanagement/domain/model/AppointmentModel.java
@@ -9,25 +9,25 @@ import lombok.Value;
 public class AppointmentModel {
 
     AppointmentId id;
-   // PatientId patientId;  se habilitara cuando la clase relacionada sea creada
-   // DoctorId doctorId;  se habilitara cuando la clase relacionda sea creada
+    PatientId patientId;
+    DoctorId doctorId;
     AppointmentDate appointmentDate;
     AppointmentReason appointmentReason;
     AppointmentStatus appointmentStatus;
 
     public static AppointmentModel create(
             final AppointmentId id,
-           // final PatientId patientId,
-           // final DoctorId doctorId,
+            final PatientId patientId,
+            final DoctorId doctorId,
             final AppointmentDate appointmentDate,
             final AppointmentReason appointmentReason) {
-        return new AppointmentModel(id, appointmentDate, appointmentReason, AppointmentStatus.SCHEDULED);
+        return new AppointmentModel(id, patientId, doctorId, appointmentDate, appointmentReason, AppointmentStatus.SCHEDULED);
     }
 
     public AppointmentModel completed() {
-        return new AppointmentModel(id, appointmentDate, appointmentReason, AppointmentStatus.COMPLETED);
+        return new AppointmentModel(id, patientId, doctorId, appointmentDate, appointmentReason, AppointmentStatus.COMPLETED);
     }
     public AppointmentModel canceled() {
-        return new AppointmentModel(id, appointmentDate, appointmentReason, AppointmentStatus.CANCELED);
+        return new AppointmentModel(id, patientId, doctorId, appointmentDate, appointmentReason, AppointmentStatus.CANCELED);
     }
 }
